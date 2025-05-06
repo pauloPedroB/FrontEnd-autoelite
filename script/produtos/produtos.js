8e78b030c077c88dae7696ea094117ec2e21eec5
lucas <ahissareis@gmail.com>
2025-05-06 19:36:35 -0300

    att

--- script/produtos/produtos.js
+++ script/produtos/produtos.js
@@ -3,7 +3,10 @@ const API_URL = "http://localhost:3001/produtos/";
 
 
 window.addEventListener('load', async function() {
+  const params = new URLSearchParams(window.location.search);
+
   const usuario = buscarUsuario()
+  const termo = params.get("pesquisa");
   
   if (usuario) {
     if(usuario.typeUser != 3){
@@ -12,9 +15,23 @@ window.addEventListener('load', async function() {
         const elemento = document.getElementById('myProducts');
         elemento.remove();
       }
-      const [produtos, message] = await listar();
-      getCarregar(produtos);
+      let palavras = []
+      if(termo){
+        const resposta = await fetch('http://localhost:5000/limpar', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ texto: termo })
+        });
 
+        const dados = await resposta.json();
+        palavras = dados.palavras;
+      }
+      const [produtos, message] = await listar(palavras);
+
+      
+      getCarregar(produtos);  
 
       // Carregar dados, exibir conteúdo, etc.
     }
@@ -58,12 +75,12 @@ async function listar(nomes = [], categoria = null){
 const getCarregar = function(produtos){
 
   let div_produtos = document.getElementById("cards-not-linked")
+  const usuario = buscarUsuario()
 
   for (const produto of produtos){
       let div_caixa_produto = document.createElement('div')
       let h2_caixa_produto = document.createElement('h3')
       let img = document.createElement('img')
-      let button = document.createElement('button')
 
       div_caixa_produto.setAttribute('class', 'card-not-linked')
 
@@ -74,10 +91,25 @@ const getCarregar = function(produtos){
       div_produtos.appendChild(div_caixa_produto)
       div_caixa_produto.appendChild(h2_caixa_produto)
       div_caixa_produto.appendChild(img)
-      div_caixa_produto.appendChild(button)
 
       h2_caixa_produto.innerText = produto.nome_produto;
-      button.innerText = "vincular";
+      if(usuario.typeUser == 2){
+
+        let button = document.createElement('button')
+
+        div_caixa_produto.appendChild(button)
+        button.innerText = "vincular";
+      }
+      else if(usuario.typeUser == 1){
+        let button1 = document.createElement('button')
+        div_caixa_produto.appendChild(button1)
+        button1.innerText = "Editar";
+
+        let button2 = document.createElement('button')
+        div_caixa_produto.appendChild(button2)
+        button2.innerText = "Excluir";
+
+      }
 
   }
 
