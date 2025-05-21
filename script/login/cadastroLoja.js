4ae2c65e7b94fdc96d8226edacb9af95366f1251
lucas <ahissareis@gmail.com>
2025-05-21 20:22:22 -0300

    att

---
+++ script/login/cadastroLoja.js
@@ -0,0 +1,26 @@
+import { buscarUsuario } from '../middleware/auth.js';
+
+const API_URL = "http://localhost:3001/usuarios/";
+const token = sessionStorage.getItem('token');
+
+
+
+window.addEventListener('load', async function() {
+    if(token){
+        const usuario = buscarUsuario()
+        if(usuario.typeUser != null){
+            window.location.href = "/view/home.html";
+        }
+    }
+    else{
+        window.location.href = "/view/home.html";
+    }
+
+    const form = document.getElementById('encerrarSessao');
+    form.addEventListener('submit', async function (event) {
+        event.preventDefault(); // Evita o envio padrão do formulário
+        sessionStorage.clear();
+        form.submit();
+    });
+
+});
